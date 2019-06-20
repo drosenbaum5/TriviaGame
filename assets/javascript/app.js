d70f5f8301b0e407968c91ebc0ed7a4c9fc1efa6
Dan Rosenbaum <drosenbaum5@gmail.com>
2019-06-20 19:29:39 -0400

    Changed CSS styling. Removed hide feature

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,16 +6,35 @@ var interval_Id;
 //                         secondQuestion S: }
 
 
-//Start the timer when the title button is clicked
-$("#title-button").click(timerStart);
 
-//Sets interval for timer to count back from 16. First number displayed is 15.
+
+
+//Start the timer when the title button is clicked
+    $(document).ready(function(){
+          $("#title-button").click(function() {
+          $("#title-button").replaceWith("");  
+          displayQuestions();
+          timerStart();
+          callNewQuestion();
+          
+          
+       
+          
+        });
+      });
+    
+function displayQuestions() {
+
+   $("#questions").show();
+}
+ 
+//Sets interval for timer to count back by 1 second
 function timerStart() {
 
   interval_Id = setInterval(decrement, 1000);
 }
 
-
+//Decrements displayed time. If timer reaches 0, timer stops.
 function decrement () {
  timer--
  $("#timer").html("<p>" + timer + "</p>");
@@ -26,10 +45,45 @@ function decrement () {
  } 
 }
 
+//function to stop the timer when it hits 0
 function stop() {
 
   clearInterval(interval_Id);
 }
 
+
+//main function for calling new question and answers
+ function callNewQuestion () {
  
+      $("#questions").replaceWith("Hello world!");
+            
+            callNewAnswer1();
+            callNewAnswer2();
+            callNewAnswer3();
+            callNewAnswer4();
+
+
+   function callNewAnswer1 () {
+
+      $("#answer-one").replaceWith("<h3>Does this work?</h3>") 
+   }
+      //inserts new question 2
+   function callNewAnswer2 () {
+
+      $("#answer-two").replaceWith("<h3>Does this work?</h3>") 
+   }
+       //inserts new question 3
+   function callNewAnswer3 () {
+
+      $("#answer-three").replaceWith("<h3>Does this work?</h3>") 
+   }
+      //inserts new question 4
+   function callNewAnswer4 () {
+
+      $("#answer-four").replaceWith("<h3>Does this work?</h3>") 
+   }
+
+   
+   
+}
  
