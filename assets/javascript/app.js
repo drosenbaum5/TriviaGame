cc23d560e3580f615d39dfcf1ff6342447e1db9b
Dan Rosenbaum <drosenbaum5@gmail.com>
2019-06-20 20:24:07 -0400

    Moved timer to sit below answers. Moved Question to display in top most container.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,13 @@
 var correctGuesses;
 var incorrectGuesses;
-var timer = 16;
+var timer = 34;
 var interval_Id;
 // var triviaQuestions = { firstQuestion : "Who attempted to assassinate Commander Adama in the season 1 finale?",
 //                         secondQuestion S: }
 
+
+var questionOne = "<u><h1>Who attempted to assassinate Commander Adama?</h1></u>";
+var answers = ["Lee Adama", "Caprica Six", "Sharon", "Colonel Tigh"]
 
 
 
@@ -12,10 +15,10 @@ var interval_Id;
 //Start the timer when the title button is clicked
     $(document).ready(function(){
           $("#title-button").click(function() {
-          $("#title-button").replaceWith("");  
-          displayQuestions();
+          $("#title-button").replaceWith(questionOne);  
+        
           timerStart();
-          callNewQuestion();
+          callNewQuestions();
           
           
        
@@ -23,10 +26,7 @@ var interval_Id;
         });
       });
     
-function displayQuestions() {
 
-   $("#questions").show();
-}
  
 //Sets interval for timer to count back by 1 second
 function timerStart() {
@@ -53,34 +53,38 @@ function stop() {
 
 
 //main function for calling new question and answers
- function callNewQuestion () {
+ function callNewQuestions () {
  
-      $("#questions").replaceWith("Hello world!");
-            
+      
+            addQuestion();
             callNewAnswer1();
             callNewAnswer2();
             callNewAnswer3();
             callNewAnswer4();
 
+   function addQuestion () {
+
+      $("#questions").replaceWith("Hello world!");
+   }
 
    function callNewAnswer1 () {
 
-      $("#answer-one").replaceWith("<h3>Does this work?</h3>") 
+      $("#answer-one").replaceWith("<h3>" + answers[0] + "</h3>") 
    }
       //inserts new question 2
    function callNewAnswer2 () {
 
-      $("#answer-two").replaceWith("<h3>Does this work?</h3>") 
+      $("#answer-two").replaceWith("<h3>" + answers[1] + "</h3>") 
    }
        //inserts new question 3
    function callNewAnswer3 () {
 
-      $("#answer-three").replaceWith("<h3>Does this work?</h3>") 
+      $("#answer-three").replaceWith("<h3>" + answers[2] + "</h3>") 
    }
       //inserts new question 4
    function callNewAnswer4 () {
 
-      $("#answer-four").replaceWith("<h3>Does this work?</h3>") 
+      $("#answer-four").replaceWith("<h3>" + answers[3] + "</h3>") 
    }
 
    
